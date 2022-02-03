41d55bf878c4b978fcabfc72695c8be8302df3d4
7flyx <2647282717@qq.com>
2022-02-03 19:53:42 +0800

    自动化测试练习

--- 20220104/demo1.py
+++ 20220104/demo1.py
@@ -2,7 +2,6 @@
 # 注意点：调用函数时，函数的定义必须在前面，跟C类似
 import random
 
-
 def hello():
     myName = input('请输入姓名') # input输入函数，里面可以写提示信息。input返回的默认是字符串
     print('hello world,'+ myName + '身高：'+ height + ' 体重：'+ weight)
@@ -20,8 +19,6 @@ def string_demo():
     line = '+' * 40 # 直接创建40个+号
     words = [word1, word2, word3] # 【】可以建列表，类似于数组
     print(line + words[random.randint(0, 2)] + line)
-
-
 
 num1 = input('请输入一个数字:')
 print('你输入的是数字%s'%num1,'可它的类型为：',type(num1)) #17、输出函数格式控制
